9eeeb4471c742b8526f815482bff099b4cff0fc1
Risini-33 <137677342+Risini-33@users.noreply.github.com>
2023-06-25 14:44:29 +0530

    Add files via upload

---
+++ Lab3-2023.05.26/Q2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string vowels = "aeiouAEIOU";
+            Console.Write("Enter your string : ");
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (vowels.Contains(str[i]))
+                {
+                    num++;
+                }
+            }
+
+            Console.WriteLine($"\nNumber of vowels in the given string is : {num}");
+
+            Console.ReadKey();
+        }
+    }
+}
